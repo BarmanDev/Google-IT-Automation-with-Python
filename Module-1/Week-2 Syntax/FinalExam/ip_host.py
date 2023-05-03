b04ecbde03f345bba177560988f1ee81b5c1d355
Barmandev <elbarmandev@gmail.com>
2023-05-03 13:15:09 +0200

    Finalizado

---
+++ Module-1/Week-2 Syntax/FinalExam/ip_host.py
@@ -0,0 +1,4 @@
+IP_address = "192.168.1.10"
+host_name = "Printer Server 1"
+print(IP_address + " is the IP address of " + host_name)
+# Should print "192.168.1.10 is the IP address of Printer Server 1"
