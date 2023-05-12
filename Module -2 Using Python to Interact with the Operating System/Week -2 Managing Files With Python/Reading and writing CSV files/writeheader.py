c3b5198f73e8bbfc0b56364e283a82cac7c499bb
Barmandev <elbarmandev@gmail.com>
2023-05-12 16:22:58 +0200

    Finalizado

--- Module -2 Using Python to Interact with the Operating System/Week -2 Managing Files With Python/Reading and writing CSV files/writeheader.py
+++ Module -2 Using Python to Interact with the Operating System/Week -2 Managing Files With Python/Reading and writing CSV files/writeheader.py
@@ -1,11 +1,15 @@
 import csv
+# Creamos el dicionario con los datos que queremos
 users = [{"name": "Sol Mansi", "username": "solm", "department": "ItInfrastructure"},
          {"name": "Lio Nelson", "username": "lion",
              "department": "User Experience Research"},
          {"name": "Charlie Grey", "username": "greyc", "department": "Development"}]
-
+# Almacenamos las claves que queremos escribir en el archivo csv
 keys = ["name", "username", "department"]
+# Abrimos el archivo
 with open('y_deparment.csv', 'w') as by_department:
+    # Creamos el 'DicWriter' pasando las claves que escribimos
     writer = csv.DictWriter(by_department, fieldnames=keys)
-    writer.writeheader()
+    writer.writeheader()  # -> Crea la primera linea basado en las claves
+    # -> Convierte el dicionario en lineas en el archivo
     writer.writerows(users)
